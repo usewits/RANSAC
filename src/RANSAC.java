a89090865a4b959574c98d48ebf98340697a2fe5
RockGiant <RockGiant@users.noreply.github.com>
2015-01-14 13:27:31 +0100

    Removed Voronoi library, added mathetmatical variant for finding Circle.
    The program now claims to find circles on both data files. Edit the run/0
    method (last method) to change the runtime.

--- src/RANSAC.java
+++ src/RANSAC.java
@@ -59,6 +59,70 @@ class RANSAC {
 		@Override public String toString(){
 			return "Circle at (" + center.toString() + "), with radius " + Double.toString(radius);
 		}
+		/** Algorithm stolen from http://paulbourke.net/geometry/circlesphere/*/
+		public static Circle fromPoints(final Point p1,final Point p2,final Point p3){
+			if (!isPerpendicular(p1,p2,p3)) return calcCircle(p1,p2,p3);
+			else if (!isPerpendicular(p1, p3, p2)) return calcCircle(p1, p3, p2);	
+			else if (!isPerpendicular(p2, p1, p3)) return calcCircle(p2, p1, p3);	
+			else if (!isPerpendicular(p2, p3, p1)) return calcCircle(p2, p3, p1);	
+			else if (!isPerpendicular(p3, p2, p1)) return calcCircle(p3, p2, p1);	
+			else if (!isPerpendicular(p3, p1, p2)) return calcCircle(p3, p1, p2);	
+			else throw new AssertionError("Some of the points are perpendicular to some axis!");
+		}
+		/** Check for the given points if they are perpendicular to x or y axis.*/
+		private static boolean isPerpendicular(final Point p1,final Point p2,final Point p3){
+			// the original method works by comparing delta_x with a very small value
+			// here I compare the two values using Double.compare and act if they
+			// are compare equal.
+			final boolean yDeltaA = Double.compare(p2.y,p1.y) == 0;
+			final boolean xDeltaA = Double.compare(p2.x,p1.x) == 0;
+			final boolean yDeltaB = Double.compare(p3.y,p2.y) == 0;
+			final boolean xDeltaB = Double.compare(p3.x,p2.x) == 0;
+			// checking whether the line of the two points is vertical
+			// three points are perpendicular to a certain axis if
+			// there exists only! one couple of points that are on
+			// one line. If there exist two such lines (if three points
+			// are on a triangle with right angle aligned with the axes), 
+			// then the points are not considered to be perpendicular 
+			// to one axis.
+			if (xDeltaA && yDeltaB) return false;
+			if (yDeltaA) return true;
+			if (yDeltaB) return true;
+			if (xDeltaA) return true;
+			if (xDeltaB) return true;
+			return false;
+		}
+		/** Creates the circle using three points.*/
+		private static Circle calcCircle(final Point p1,final Point p2,final Point p3){
+			final double yDeltaA = p2.y - p1.y;
+			final double xDeltaA = p2.x - p1.x;
+			final double yDeltaB = p3.y - p2.y;
+			final double xDeltaB = p3.x - p2.x;
+			
+			if (Double.compare(p2.x,p1.x)==0 && Double.compare(p3.y,p2.y) == 0){
+				// this is the case where the three points lie on a triangle with
+				// a right angle, and lie parallel to the axes.
+				final double circleX = .5 * (p2.x + p3.x);
+				final double circleY = .5 * (p1.y + p2.y);
+				final Point centerPoint = new Point(circleX,circleY);
+				final double radius = Point.euclDistance(centerPoint,p1);
+				return new Circle(centerPoint,radius);
+			}
+			final double aSlope = yDeltaA / xDeltaA;
+			final double bSlope = yDeltaB / xDeltaB;
+			if (Double.compare(aSlope,bSlope)==0)
+				throw new AssertionError("The points are colinear!");
+			final double circleX = 
+					(aSlope*bSlope*(p1.y - p3.y) + 
+							bSlope*(p1.x+p2.x) - 
+							aSlope*(p2.x+p3.x)) / 
+							(2*(bSlope-aSlope));
+			final double circleY = 
+					-1*(circleX - (p1.x+p2.x)/2)/aSlope +  (p1.y+p2.y)/2;
+			final Point centerPoint = new Point(circleX,circleY);
+			final double radius = Point.euclDistance(centerPoint,p1);
+			return new Circle(centerPoint,radius);
+		}
 	}
 	
 	private static final class Model {
@@ -71,7 +135,7 @@ class RANSAC {
 		public Circle getCircle(){return circle;}
 		public int getNumberOfInliers(){return numberOfInliers;}
 		@Override public String toString(){
-			return circle.toString() + " with " + Integer.toString(numberOfInliers) + " number of inliers";
+			return circle.toString() + " with " + Integer.toString(numberOfInliers) + " inliers";
 		}
 	}
 	
@@ -95,19 +159,6 @@ class RANSAC {
 						return -1*(arg0.getNumberOfInliers() - arg1.getNumberOfInliers());
 					}
 				}).maximumSize(10).create();
-		//Model bestModel = null;
-		final Voronoi voronoiAlgorithm = new Voronoi(0); //TODO: 0 is the minDistance between points, valid?
-		// determine bounding box!
-		double minX = Double.POSITIVE_INFINITY;
-		double maxX = Double.NEGATIVE_INFINITY;
-		double minY = Double.POSITIVE_INFINITY;
-		double maxY = Double.NEGATIVE_INFINITY;
-		for(final Point p : input){
-			if (p.x < minX) minX = p.x;
-			if (p.x > maxX) maxX = p.x;
-			if (p.y < minY) minY = p.y;
-			if (p.y > maxY) maxY = p.y;
-		}
 		for(int i=0;i<iterations;i++){
 			// randomly select three Points
 			// Points must be distinct!
@@ -120,47 +171,11 @@ class RANSAC {
 			do{
 				index3 = (int)(input.size() * Math.random());
 			}while(index3 == index1 || index3 == index2);
-			Point[] randomPoints = new Point[3];
-			randomPoints[0] = input.get(index1);
-			randomPoints[1] = input.get(index2);
-			randomPoints[2] = input.get(index3);
-			// prepare argument for Voronoi algorithm
-			final double[] xs = new double[3];
-			final double[] ys = new double[3];
-			int index = -1;
-			for(final Point p : randomPoints){
-				++index;
-				xs[index] = p.x;
-				ys[index] = p.y;
-			}
-			final List<GraphEdge> edgeList = voronoiAlgorithm.generateVoronoi(xs,ys,minX,maxX,minY,maxY);
-			// the center of the circle lies at the intersection
-			final Map<Point,Integer> edgesCross = new HashMap<>();
-			// for all points in all edges, compute the number of occurances of each point.
-			for(final GraphEdge edge : edgeList){
-				for(final Point point : new Point[] {new Point(edge.x1,edge.y1),new Point(edge.x2,edge.y2)}){
-					if(edgesCross.computeIfPresent(point,new BiFunction<Point,Integer,Integer>(){
-						@Override
-						public Integer apply(Point arg0,Integer arg1){
-							return Integer.valueOf(arg1+1);
-						}
-					})==null)
-						edgesCross.put(point,Integer.valueOf(1));
-				}
-			}
-			// because we have three points, there is one unique point that is hit three times
-			Point center = null;
-			for(final Entry<Point,Integer> pair : edgesCross.entrySet()){
-				if (pair.getValue() == Integer.valueOf(3))
-					center = pair.getKey();
-			}
-			// radius is distance to closest point from center
-			final double radius = Point.euclDistance(center,input.get(0));
-			final Circle circle = new Circle(center,radius);
+			final Circle circle = Circle.fromPoints(input.get(index1),input.get(index2),input.get(index3));
 			// we now have the circle, now compute the inliers
 			final List<Point> inliers = new ArrayList<>();
 			for(final Point point : input){
-				double distanceToCircle = Math.abs(Point.euclDistance(center,point));
+				double distanceToCircle = Math.abs(Point.euclDistance(circle.getCenter(),point));
 				if (distanceToCircle < inlierRadius){
 					inliers.add(point);
 				}
@@ -198,8 +213,8 @@ class RANSAC {
 		final List<Point> points1 = readCSV("data/points1.5.csv");
 		final List<Point> points2 = readCSV("data/points1.6.csv");
 		
-		final List<Model> models1 = doRansac(points1,5000,1,.015);
-		final List<Model> models2 = doRansac(points2,5000,1,.015);
+		final List<Model> models1 = doRansac(points1,5000000,15,.015);
+		final List<Model> models2 = doRansac(points2,5000000,15,.015);
 		
 		System.out.println("Models found for points1:");
 		System.out.println(models1.toString());
